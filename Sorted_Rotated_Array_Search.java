a24765f3152ad521909ea4d7046ee7001d61d131
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-12-28 16:07:01 +0530

    LinearSearchInSortedAndRotatedArray

--- Sorted_Rotated_Array_Search.java
+++ Sorted_Rotated_Array_Search.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Sorted_Rotated_Array_Search {
+    public static int search(int[] arr, int target, int si, int ei) {
+        // mid
+        int mid = si + (ei - si) / 2;
+
+        // base case
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        //for l1
+        if (arr[si] <= arr[mid]) {
+            if (arr[si] <= target && target < arr[mid]) {
+                return search(arr, target, si, mid - 1); // left part searching
+            } else {
+                return search(arr, target, mid + 1, ei);
+            }
+        }
+
+        // for l2
+        else {
+            if (arr[mid] <= target && target < arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            } else {
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int tidx = search(arr, target, 0, arr.length - 1);
+        System.out.println(tidx); // 4
+    }
 }
